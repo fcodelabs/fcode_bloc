4cc2c30bb03edca96413695b3a0e0489e0eba967
ramesh <ramesh@fcodelabs.com>
2019-09-04 23:16:35 +0530

    add multi provider

---
+++ lib/bloc/multi_bloc_provider.dart
@@ -0,0 +1,15 @@
+import 'package:fcode_mvp/bloc/bloc_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MultiBlocProvider extends MultiProvider {
+  MultiBlocProvider({
+    Key key,
+    @required List<BlocProvider> providers,
+    @required Widget child,
+  }) : super(
+          key: key,
+          providers: providers,
+          child: child,
+        );
+}

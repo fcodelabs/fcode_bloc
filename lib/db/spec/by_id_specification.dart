32d6c0413f300ce1c41e4530ff1e3e7d71eb641e
ramesh <ramesh@fcodelabs.com>
2019-09-10 18:56:55 +0530

    add byidspecification

---
+++ lib/db/spec/by_id_specification.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fcode_bloc/db/specification.dart';
+
+class ByIDSpecification implements SpecificationI {
+  final String _id;
+
+  ByIDSpecification(this._id);
+
+  @override
+  Stream<List<DocumentSnapshot>> specify(CollectionReference collection) {
+    return collection.document(_id).snapshots().map<List<DocumentSnapshot>>((snapshot) => [snapshot]);
+  }
+}
